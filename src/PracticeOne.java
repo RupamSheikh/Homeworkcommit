0279fc1f89bf4f30b741c94e4c8f0bf2b8576368
rupamsheikh <sheikh.rupam@gmail.com>
2021-02-16 14:39:48 -0500

    "second commit"

--- src/PracticeOne.java
+++ src/PracticeOne.java
@@ -1,5 +1,11 @@
 public class PracticeOne {
     public static void main(String[] args) {
-        System.out.println("I can do it " );
+        System.out.println("I can do it ");
+
+        int x = 5;
+        int y = 6;
+        int sum = x + y;
+        System.out.println(sum);
     }
+
 }
