d9b603a35ccb4d6aa1d31c46f00cc656345a676c
rupamsheikh <sheikh.rupam@gmail.com>
2021-03-01 22:54:11 -0500

    "switch case commit"

--- src/ContinueStatement.java
+++ src/ContinueStatement.java
@@ -3,7 +3,7 @@ public class ContinueStatement {
         //Usage of continue statement
 
         for (int i=1; i<=10; i++){
-            if (i==5){
+            if (i==5){              //1 2 3 4 6 7 8 9 10
                 continue;
             }
             System.out.println(i);
---
+++ src/Swapping.java
@@ -0,0 +1,16 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("value of a is before swapping" + a);
+        System.out.println("value of b is before swapping" + b);
+
+        int c;
+        c=a; //c is 10
+        a=b; //a is 20
+        b=c; //b is 10
+
+        System.out.println("value of a is after swapping" + a);
+        System.out.println("value of b is after swapping" + b);
+    }
+}
